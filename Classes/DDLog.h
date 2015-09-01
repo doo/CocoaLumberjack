ba2274cbd6e69f1fdd00143cca332dfd7c3ce173
Sebastian Husche <sebastian@doo.net>
2015-09-01 19:20:41 +0200

    Fix compiler issues.

--- Classes/DDLog.h
+++ Classes/DDLog.h
@@ -16,11 +16,11 @@
 #import <Foundation/Foundation.h>
 
 // Enable 1.9.x legacy macros if imported directly
-#ifndef DD_LEGACY_MACROS
-    #define DD_LEGACY_MACROS 1
-#endif
+//#ifndef DD_LEGACY_MACROS
+//    #define DD_LEGACY_MACROS 1
+//#endif
 // DD_LEGACY_MACROS is checked in the file itself
-#import <CocoaLumberjack/DDLegacyMacros.h>
+//#import <CocoaLumberjack/DDLegacyMacros.h>
 
 #if OS_OBJECT_USE_OBJC
     #define DISPATCH_QUEUE_REFERENCE_TYPE strong
